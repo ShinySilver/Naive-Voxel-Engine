f65d596be605c3d42c4d58ff005f26c3f04b5e01
Steins7 <steinsset@gmail.com>
2021-09-12 13:33:45 +0200

    Added entity rendering
    * fixed ressource folder name
    + added basic world initialization
    + added basic entity preload and render loops

--- src/common/world.cpp
+++ src/common/world.cpp
@@ -1,14 +1,23 @@
 #include "world.h"
 
-#include "world/entities/entity.h"
+#include "world/entities/entity_uniform_voxel.h"
 
-#include <vector>
+namespace world {
 
-namespace World {
+static std::vector<Entity*> entities;
 
-static std::vector<Entity> entities;
+void init() {
 
-void init() {}
+	//temporary
+	for(int i=0; i<3; ++i) {
+		auto entity = new EntityUniformVoxel(glm::vec3(2*i, 0, 0), glm::vec4(i/3.0, 0.5, 0.5, 1));
+		entities.emplace_back(entity);
+	}
+}
+
+std::vector<Entity*>& get_entities() {
+	return entities;
+}
 
 } //namespace world
 
