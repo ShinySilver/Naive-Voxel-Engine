b352127975fc5363348927f6a965028bb55a3368
Steins7 <steinsset@gmail.com>
2023-06-02 19:14:32 +0200

    Add basic optimisations to basic mesher

--- src/client/utils/meshing/basic_mesher.cpp
+++ src/client/utils/meshing/basic_mesher.cpp
@@ -5,10 +5,11 @@
 #include <tuple>
 #include <ranges>
 #include <glm/glm/ext.hpp>
-#include <loguru.hpp>
 
 constexpr float offset = -CHUNK_SIZE * VOXEL_SIZE / 2.0f;
 constexpr float delta = VOXEL_SIZE / 2.0f;
+auto average_duration = std::chrono::duration<double> (0);
+double count = 0;
 
 namespace BasicMesher
 {
@@ -23,110 +24,104 @@ namespace BasicMesher
 						continue;
 					}
 
+					const auto x1 = x - delta;
+					const auto x2 = x + delta;
+					const auto y1 = y - delta;
+					const auto y2 = y + delta;
+					const auto z1 = z - delta;
+					const auto z2 = z + delta;
+					const auto voxel = *chunk.get(x, y, z);
+
 					if ((x == 0) || (*chunk.get(x - 1, y, z) == VOXEL_AIR))
 					{
-						mesh->vertices.emplace_back(glm::vec3(x - delta, y - delta, z - delta));
-						mesh->vertices.emplace_back(glm::vec3(x - delta, y - delta, z + delta));
-						mesh->vertices.emplace_back(glm::vec3(x - delta, y + delta, z - delta));
-						mesh->vertices.emplace_back(glm::vec3(x - delta, y + delta, z - delta));
-						mesh->vertices.emplace_back(glm::vec3(x - delta, y - delta, z + delta));
-						mesh->vertices.emplace_back(glm::vec3(x - delta, y + delta, z + delta));
+						mesh->vertices.emplace_back(glm::vec3(x1, y1, z1));
+						mesh->vertices.emplace_back(glm::vec3(x1, y1, z2));
+						mesh->vertices.emplace_back(glm::vec3(x1, y2, z1));
+						mesh->vertices.emplace_back(glm::vec3(x1, y2, z1));
+						mesh->vertices.emplace_back(glm::vec3(x1, y1, z2));
+						mesh->vertices.emplace_back(glm::vec3(x1, y2, z2));
 						for (int i = 0; i < 6; ++i) {
-							mesh->colors.emplace_back(*chunk.get(x, y, z));
+							mesh->colors.emplace_back(voxel);
 						}
 					}
 
 					if ((x == CHUNK_WIDTH - 1) || (*chunk.get(x + 1, y, z) == VOXEL_AIR))
 					{
-						mesh->vertices.emplace_back(glm::vec3(x + delta, y - delta, z - delta));
-						mesh->vertices.emplace_back(glm::vec3(x + delta, y + delta, z - delta));
-						mesh->vertices.emplace_back(glm::vec3(x + delta, y - delta, z + delta));
-						mesh->vertices.emplace_back(glm::vec3(x + delta, y + delta, z - delta));
-						mesh->vertices.emplace_back(glm::vec3(x + delta, y + delta, z + delta));
-						mesh->vertices.emplace_back(glm::vec3(x + delta, y - delta, z + delta));
+						mesh->vertices.emplace_back(glm::vec3(x2, y1, z1));
+						mesh->vertices.emplace_back(glm::vec3(x2, y2, z1));
+						mesh->vertices.emplace_back(glm::vec3(x2, y1, z2));
+						mesh->vertices.emplace_back(glm::vec3(x2, y2, z1));
+						mesh->vertices.emplace_back(glm::vec3(x2, y2, z2));
+						mesh->vertices.emplace_back(glm::vec3(x2, y1, z2));
 						for (int i = 0; i < 6; ++i) {
-							mesh->colors.emplace_back(*chunk.get(x, y, z));
+							mesh->colors.emplace_back(voxel);
 						}
 					}
 
 					if ((y == 0) || (*chunk.get(x, y - 1, z) == VOXEL_AIR))
 					{
-						mesh->vertices.emplace_back(glm::vec3(x - delta, y - delta, z - delta));
-						mesh->vertices.emplace_back(glm::vec3(x + delta, y - delta, z - delta));
-						mesh->vertices.emplace_back(glm::vec3(x - delta, y - delta, z + delta));
-						mesh->vertices.emplace_back(glm::vec3(x + delta, y - delta, z - delta));
-						mesh->vertices.emplace_back(glm::vec3(x + delta, y - delta, z + delta));
-						mesh->vertices.emplace_back(glm::vec3(x - delta, y - delta, z + delta));
+						mesh->vertices.emplace_back(glm::vec3(x1, y1, z1));
+						mesh->vertices.emplace_back(glm::vec3(x2, y1, z1));
+						mesh->vertices.emplace_back(glm::vec3(x1, y1, z2));
+						mesh->vertices.emplace_back(glm::vec3(x2, y1, z1));
+						mesh->vertices.emplace_back(glm::vec3(x2, y1, z2));
+						mesh->vertices.emplace_back(glm::vec3(x1, y1, z2));
 						for (int i = 0; i < 6; ++i) {
-							mesh->colors.emplace_back(*chunk.get(x, y, z));
+							mesh->colors.emplace_back(voxel);
 						}
 					}
 
 					if ((y == CHUNK_WIDTH - 1) || (*chunk.get(x, y + 1, z) == VOXEL_AIR))
 					{
-						mesh->vertices.emplace_back(glm::vec3(x - delta, y + delta, z - delta));
-						mesh->vertices.emplace_back(glm::vec3(x - delta, y + delta, z + delta));
-						mesh->vertices.emplace_back(glm::vec3(x + delta, y + delta, z - delta));
-						mesh->vertices.emplace_back(glm::vec3(x + delta, y + delta, z - delta));
-						mesh->vertices.emplace_back(glm::vec3(x - delta, y + delta, z + delta));
-						mesh->vertices.emplace_back(glm::vec3(x + delta, y + delta, z + delta));
+						mesh->vertices.emplace_back(glm::vec3(x1, y2, z1));
+						mesh->vertices.emplace_back(glm::vec3(x1, y2, z2));
+						mesh->vertices.emplace_back(glm::vec3(x2, y2, z1));
+						mesh->vertices.emplace_back(glm::vec3(x2, y2, z1));
+						mesh->vertices.emplace_back(glm::vec3(x1, y2, z2));
+						mesh->vertices.emplace_back(glm::vec3(x2, y2, z2));
 						for (int i = 0; i < 6; ++i) {
-							mesh->colors.emplace_back(*chunk.get(x, y, z));
+							mesh->colors.emplace_back(voxel);
 						}
 					}
 
 					if ((z == 0) || (*chunk.get(x, y, z - 1) == VOXEL_AIR))
 					{
-						mesh->vertices.emplace_back(glm::vec3(x - delta, y - delta, z - delta));
-						mesh->vertices.emplace_back(glm::vec3(x - delta, y + delta, z - delta));
-						mesh->vertices.emplace_back(glm::vec3(x + delta, y - delta, z - delta));
-						mesh->vertices.emplace_back(glm::vec3(x + delta, y - delta, z - delta));
-						mesh->vertices.emplace_back(glm::vec3(x - delta, y + delta, z - delta));
-						mesh->vertices.emplace_back(glm::vec3(x + delta, y + delta, z - delta));
+						mesh->vertices.emplace_back(glm::vec3(x1, y1, z1));
+						mesh->vertices.emplace_back(glm::vec3(x1, y2, z1));
+						mesh->vertices.emplace_back(glm::vec3(x2, y1, z1));
+						mesh->vertices.emplace_back(glm::vec3(x2, y1, z1));
+						mesh->vertices.emplace_back(glm::vec3(x1, y2, z1));
+						mesh->vertices.emplace_back(glm::vec3(x2, y2, z1));
 						for (int i = 0; i < 6; ++i) {
-							mesh->colors.emplace_back(*chunk.get(x, y, z));
+							mesh->colors.emplace_back(voxel);
 						}
 					}
 
 					if ((z == CHUNK_HEIGHT - 1) || (*chunk.get(x, y, z + 1) == VOXEL_AIR))
 					{
-						mesh->vertices.emplace_back(glm::vec3(x - delta, y - delta, z + delta));
-						mesh->vertices.emplace_back(glm::vec3(x + delta, y - delta, z + delta));
-						mesh->vertices.emplace_back(glm::vec3(x - delta, y + delta, z + delta));
-						mesh->vertices.emplace_back(glm::vec3(x + delta, y - delta, z + delta));
-						mesh->vertices.emplace_back(glm::vec3(x + delta, y + delta, z + delta));
-						mesh->vertices.emplace_back(glm::vec3(x - delta, y + delta, z + delta));
+						mesh->vertices.emplace_back(glm::vec3(x1, y1, z2));
+						mesh->vertices.emplace_back(glm::vec3(x2, y1, z2));
+						mesh->vertices.emplace_back(glm::vec3(x1, y2, z2));
+						mesh->vertices.emplace_back(glm::vec3(x2, y1, z2));
+						mesh->vertices.emplace_back(glm::vec3(x2, y2, z2));
+						mesh->vertices.emplace_back(glm::vec3(x1, y2, z2));
 						for (int i = 0; i < 6; ++i) {
-							mesh->colors.emplace_back(*chunk.get(x, y, z));
+							mesh->colors.emplace_back(voxel);
 						}
 					}
 				}
 			}
 		}
 
-//		for (const auto& voxel : chunk.data) {
-//			if (voxel != VOXEL_AIR) {
-//				mesh->colors.emplace_back(voxel);
-//			}
-//		}
-
 		mesh->normals.reserve(mesh->vertices.size());
 		for (int i = 0; i < mesh->vertices.size(); i += 3) {
-			mesh->normals.emplace_back(
-					glm::normalize(glm::cross(
+			const auto normal = glm::normalize(glm::cross(
 						mesh->vertices[i + 2] - mesh->vertices[i + 0],
 						mesh->vertices[i + 1] - mesh->vertices[i + 0]
-					)));
-			mesh->normals.emplace_back(
-					glm::normalize(glm::cross(
-						mesh->vertices[i + 2] - mesh->vertices[i + 0],
-						mesh->vertices[i + 1] - mesh->vertices[i + 0]
-					)));
-			mesh->normals.emplace_back(
-					glm::normalize(glm::cross(
-						mesh->vertices[i + 2] - mesh->vertices[i + 0],
-						mesh->vertices[i + 1] - mesh->vertices[i + 0]
-					)));
+					));
+			mesh->normals.emplace_back(normal);
+			mesh->normals.emplace_back(normal);
+			mesh->normals.emplace_back(normal);
 		}
 
 		return mesh;
